5c90ef29223cf216b094e129042f56661ba114d3
chesterchenn <chesterchenn@gmail.com>
2021-02-27 12:59:56 +0800

    refactor: statement DEST

--- bakup.sh
+++ bakup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # 用于自动备份的脚本
 
-# 指定需要备份的文件
+# 指定需要备份的文件和目标文件夹
 FILES=(~/.vimrc)
+DEST="."
 
 for file in ${FILES[@]}
 do
   if [ -f "$file" ];then
-    echo -e "\033[32mcopy $file to current folder \033[0m"
-    cp "$file" .
+    echo -e "\033[32mcopy $file to $DEST\033[0m"
+    cp $file $DEST
   else
     echo -e "\033[31m$file don't exist\033[0m"
   fi
