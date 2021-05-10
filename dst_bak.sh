2c7c246d42ee7fdb85b9200cb3c92a5e434ddf4e
chesterchenn <chesterchenn@gmail.com>
2021-05-10 15:18:34 +0800

    fix: namespace conflict

--- dst_bak.sh
+++ dst_bak.sh
@@ -7,12 +7,12 @@
 # id=1234
 # cluster=Cluster_1
 
-conf=~/.dst_conf
+confFile=~/.dst_conf
 fileName=饥荒存档$(date "+%Y%m%d").zip
 prefix=~
 
 # 判断 .dst_conf 文件是否存在
-test ! -e $conf && echo "$conf 文件不存在" && exit 0
+test ! -e $confFile && echo "$confFile 文件不存在" && exit 0
 
 conf=$(cat $conf)
 
@@ -25,10 +25,11 @@ _cluster=$(echo "$conf" | grep "cluster=" | cut -d "=" -f 2)
 _path=$prefix/$_id
 
 # 判断目标文件夹是否存在
-test ! -e $_path && echo "$_path 存档不存在" && exit 0
+test ! -e $_path && echo "$_path/$_cluster 存档不存在" && exit 0
 
-echo "进行存档的目录：$_path"
+echo "进行存档的目录：$_path/$_cluster"
 
+# 进入目录，进行压缩
 $(cd $_path; zip -Jqr ~/$fileName $_cluster)
 
 # 判断文件是否存在
