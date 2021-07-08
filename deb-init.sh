7495151de886cc57d0ddaa4b1bcfff8eae14d245
chesterchenn <chesterchenn@outlook.com>
2021-07-08 13:43:56 +0800

    feat: add determine

--- deb-init.sh
+++ deb-init.sh
@@ -2,12 +2,20 @@
 
 # 用于安装 debian 系的软件
 echo "开始安装软件"
-sudo apt install -y vim git proxychains4 zsh neofetch
+sudo apt install -y vim git proxychains4 zsh neofetch ranger
 
 # 更新 shell
 echo "更新 shell"
-chsh -s /usr/bin/zsh
+if [ $SHELL != "/usr/bin/zsh" ]; then
+  chsh -s /usr/bin/zsh
+fi
+echo "当前 shell 为 $SHELL"
 
 # 生成秘钥
 echo "生成秘钥"
-ssh-keygen
+sshFile=~/.ssh/id_rsa
+if [ -e $sshFile ]; then
+  echo "$sshFile 已经存在"
+else
+  ssh-keygen
+fi
