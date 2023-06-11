21cc75faa4f739bf49f49dea24b6685c82bfd5e0
chesterchenn <chesterchenn@gmail.com>
2023-06-11 10:20:35 +0800

    feat: 🔨恢复配置脚本

--- recovery.sh
+++ recovery.sh
@@ -39,3 +39,36 @@ else
   sh -c "$($proxy curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   echo "ohmyzsh 安装结束\n"
 fi
+
+# 众神归位
+echo "开始恢复配置"
+echo "恢复coc-settings"
+mkdir -p $HOME/.config/nvim
+cp ./coc-settings.json $HOME/.config/nvim/coc-settings.json
+
+echo "开始恢复init.vim"
+cp ./init.vim $HOME/.config/nvim/init.vim
+
+echo "开始恢复markdownlintrc"
+cp ./markdownlintrc $HOME/.markdownlintrc
+
+echo "开始恢复my_rc"
+if [ -e $HOME/.my_rc ]; then
+  echo "my_rc 已经存在\n"
+else
+  cp ./my_rc $HOME/.my_rc
+fi
+
+echo "开始prettierc"
+cp ./prettierrc $HOME/.prettierrc
+
+echo "开始恢复ranger配置"
+mkdir -p $HOME/.config/ranger
+cp ./rc.conf $HOME/.config/ranger/rc.conf
+cp ./ringer.conf $HOME/.config/ranger/ringer.conf
+
+echo "开始恢复vimrc"
+cp ./vimrc $HOME/.vimrc
+
+echo "开始恢复zshrc"
+cp ./zshrc $HOME/.zshrc
